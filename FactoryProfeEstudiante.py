895e8b8964d954bf3445b8cb828807068a341fe8
candycubillo <candycubillo1@gmail.com>
2023-04-24 15:01:42 -0600

    Dentro de la clase factory se arregla un if para que retorne el tipo sea profesor o estudiante

--- FactoryProfeEstudiante.py
+++ FactoryProfeEstudiante.py
@@ -4,7 +4,6 @@ from ClaseProfesor import Profesor
 from ClaseEstudiante import Estudiante
 #se crea clase factory matricula
 class FactoryMatricula:
-
     # se crea metodo factory para retornar un objeto de la clase
     # Instancia de la clase que va a retornar sea de tipo profesor o estudiante
     def CrearProEst(self, tipo):
