fd1c4c3145e6da346c6c445bc3ec49d9917b3b22
candycubillo <candycubillo1@gmail.com>
2023-04-24 14:59:52 -0600

    Se realiza un ajuste en los atributos de estudiante

--- ClaseEstudiante.py
+++ ClaseEstudiante.py
@@ -1,7 +1,6 @@
 from ClaseProfesor import ProfeEstudiante # se importa claseProfesor y clase Profeestudiante
 # Clase Estudiante que guarda la información de los estudiantes de la institución
 class Estudiante(ProfeEstudiante): # SE CREA CLASE ESTUDIANTE QUE HEREDA DE LA CLASE metodos de la clase Profestudiante
-
     #constructor de la clase
     def _init_(self, cedula, nombre, fechaNacimiento, nCelular, grupo): #ATRIBUTOS DE LA CLASE
         self.cedula = cedula  # CEDULA DEL ESTUDIANTE
@@ -9,5 +8,3 @@ class Estudiante(ProfeEstudiante): # SE CREA CLASE ESTUDIANTE QUE HEREDA DE LA C
         self.fechaNacimiento = fechaNacimiento  # FECHA DE NACIMIENTO DEL ESTUDIANTE
         self.nCelular = nCelular  # NUMERO DE CELULAR DEL ESTUDIANTE
         self.grupo = grupo  # GRUPO/CLASeDELESTUDIANTE
-
-
